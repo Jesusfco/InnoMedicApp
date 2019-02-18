0d4a58a40cfe68bc99fdbc001023b300a41dd1d7
Jesús Fco Cortés <jfcr@live.com>
2019-02-18 17:32:42 -0600

    Url Server Updating

---
+++ app/src/main/java/com/example/innomedicapp/util/ServerUrl.java
@@ -0,0 +1,11 @@
+package com.example.innomedicapp.util;
+
+public class ServerUrl {
+    public static String getUrlApi(){
+        return "http://inno-medic.roguezservices.com/api/";
+    }
+
+    public static String getUrl(){
+        return "http://inno-medic.roguezservices.com/";
+    }
+}
