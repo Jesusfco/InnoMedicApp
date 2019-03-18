09f5716028b56f4149b1222a595a681e1028cb48
Jesús Fco Cortés <jfcr@live.com>
2019-03-17 18:32:06 -0600

    Initian with Message component

--- app/src/main/java/com/example/innomedicapp/MonitoringActivity.java
+++ app/src/main/java/com/example/innomedicapp/MonitoringActivity.java
@@ -1,31 +1,44 @@
 package com.example.innomedicapp;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
+
+import com.example.innomedicapp.fragments.PerfilFragment;
 import com.example.innomedicapp.model.User;
 import com.example.innomedicapp.model.UserAssosiation;
 import com.example.innomedicapp.thread.LastLocationChildThread;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.Serializable;
 import java.util.Observable;
 import java.util.Observer;
 
-public class MonitoringActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MonitoringActivity extends FragmentActivity implements OnMapReadyCallback  {
 
-    private GoogleMap mMap;
+
     private LastLocationChildThread threadLocalitation;
     private static Thread localizationThread;
     private User user;
+    View mView;
+    MapView mMapView;
+    GoogleMap mMap;
+
+    TextView lastCoordanates, messageCreator, lastMessage, lastMessageTime, ppm, ppmTimeStamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +46,27 @@ public class MonitoringActivity extends FragmentActivity implements OnMapReadyCa
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_monitoring );
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById( R.id.map );
-
-        mapFragment.getMapAsync( this );
-
-
         try {
             user = (User) getIntent().getSerializableExtra( "user" );
         }catch (Exception e) {
             System.out.println(e);
         }
+
+
+        mMapView = (MapView)findViewById(  R.id.map);
+
+        if(mMapView != null) {
+            mMapView.onCreate( null );
+            mMapView.onResume();
+            mMapView.getMapAsync( this );
+        }
+
+        this.lastCoordanates = (TextView) findViewById( R.id.lastCoordanates );
+        this.messageCreator = (TextView) findViewById( R.id.messageCreator );
+        this.lastMessage = (TextView) findViewById( R.id.lastMessage );
+        this.lastMessageTime = (TextView) findViewById( R.id.lastMessageTime );
+        this.ppm = (TextView)findViewById( R.id.ppm );
+        this.ppmTimeStamp = (TextView)findViewById( R.id.ppmTimeStamp );
 
         this.threadLocalitation = new LastLocationChildThread( this, this.user.getId());
         this.threadLocalitation.addObserver( new Observer() {
@@ -66,6 +89,7 @@ public class MonitoringActivity extends FragmentActivity implements OnMapReadyCa
                     mMap.addMarker( new MarkerOptions().position( coordenates ).title( "Localizacion de " + user.getName()) ).showInfoWindow();
                     mMap.animateCamera( CameraUpdateFactory.newLatLngZoom( coordenates, 14.5f ) );
 
+                    lastCoordanates.setText( json.getString( "created_at" ) );
 
                 } catch (Exception e) {
                     System.out.println(e
@@ -81,18 +105,15 @@ public class MonitoringActivity extends FragmentActivity implements OnMapReadyCa
 
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.mMap = googleMap;
+        MapsInitializer.initialize( this );
+        mMap = googleMap;
+    }
+
+    public void chatView(View view){
+        Intent newActivity = new Intent(this, ChatActivity.class);
+        startActivity(newActivity);
+
     }
 }
